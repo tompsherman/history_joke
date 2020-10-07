12590ac61027654c21c41b7818b5080046cd10b1
unknown <tompsherman@gmail.com>
2020-10-07 10:39:35 -0400

    initial setup and scaffold

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>History is a Joke</h1>
+      <button>About</button>
+      <button>Purchase</button>
+      <button>Tell me a joke!</button>
+
+      <img src="" alt="history is a joke hero image" />
+
+      <div className="image-text-sections">
+        <div className="text-sections">
+          <h2>hey put some text in here!</h2>
+          <button>and an action button!</button>
+        </div>
+        <div className="image-sections">
+          <img src="" alt="joke image" />
+        </div>
+      </div>
+
+      <div className="image-text-sections">
+        <div className="text-sections">
+          <h2>hey put some text in here!</h2>
+          <button>and an action button!</button>
+        </div>
+        <div className="image-sections">
+          <img src="" alt="joke image" />
+        </div>
+      </div>
+
+      <div className="image-text-sections">
+        <div className="text-sections">
+          <h2>hey put some text in here!</h2>
+          <button>and an action button!</button>
+        </div>
+        <div className="image-sections">
+          <img src="" alt="joke image" />
+        </div>
+      </div>
+
+      <div>
+        <p>follow History is a Joke on Twitter or Facebook!</p>
+        <p>Dogless America copyright 2020</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
