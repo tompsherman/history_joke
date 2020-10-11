b890fe550aba330c6e5a7c56be31d711738a0d0e
unknown <tompsherman@gmail.com>
2020-10-11 07:02:54 -0400

    random joke data scaffolded

---
+++ src/data/randomjoke.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        joke: "this is a history joke",
+        explanation:
+          "this is an explanation",
+    },
+    {
+        joke: "this is a history joke",
+        explanation:
+          "this is an explanation",
+    },
+    {
+        joke: "this is a third history joke",
+        explanation:
+          "this is a third explanation",
+    },
+]
