fae98cee45efd6d3b5281b9d0c05a54e563d579c
unknown <tompsherman@gmail.com>
2020-10-11 11:42:09 -0400

    toggle set up for random joke generator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./App.css";
 
 const App = () => {
+  const [toggle, setToggle] = useState(false)
+
+  const randomJoke = () => {
+    setToggle(true)  
+  }
+
   return (
     <div className="App">
       <h1>History is a Joke</h1>
       <button>About</button>
       <button>Purchase</button>
-      <button>Tell me a joke!</button>
+      <button onClick={randomJoke}>Tell me a joke!</button>
       <br></br>
+      <div>
+        {
+          toggle ? (
+              <div>
+                <h3>So you want a joke, eh?</h3>
+                <p> this is the joke </p>
+                <p> this is the explanation</p>
+              </div>
+            ) : (null)
+        }
+      </div>
       <br></br>
       <img className="hero-image"
         src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/95733817_2604194516567196_2046409293565001728_o.jpg?_nc_cat=111&_nc_sid=e3f864&_nc_ohc=QXEEz4KLvf8AX9iGB8R&_nc_ht=scontent-iad3-1.xx&oh=ade0cf1d4ee02b7326213943b87aa7f8&oe=5FA37A10"
@@ -29,7 +46,7 @@ const App = () => {
           />
         </div>
       </div>
-
+      
       <div className="image-text-sections-alternate">
         <div className="text-sections-alt">
           <h2>hey put some text in here!</h2>
