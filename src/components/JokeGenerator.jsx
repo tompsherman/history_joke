fa165a9dff6b240444a6bd4837b5ee177c382251
unknown <tompsherman@gmail.com>
2020-12-13 19:36:51 -0500

    re-worked the random joke generator

--- src/components/JokeGenerator.jsx
+++ src/components/JokeGenerator.jsx
@@ -14,26 +14,23 @@ const JokeGenerator = () => {
       let result = jokedata[index]
       setJoke(result)
       setIndex(index < 3 ? (index + 1) : (0))
-    }    
+    }
+    
+    const jokeAlert = () => {
+      alert(
+        `${joke.jokeQ}\n\n${joke.jokeA}\n\n${joke.explanation}`
+      )
+    }
+
     return (
-        <div>
-        <div className="nav-button" onClick={randomJoke}>Tell me a joke!</div>
-        <br></br>
-        <div className="rando-joke">
-                {
-                  toggle ? (
-                      <div>
-                        <h3>So you want a joke, eh?</h3>
-                        <p>{joke.jokeQ}</p>
-                        <p>{joke.jokeA}</p>
-                        <p>{joke.explanation}</p>
-                         { index === 0?
-                           (<a href={joke.url}>get the book</a>):(null)}
-                      </div>
-                    ) : (null)
-                }
-            </div>                    
+      <div>
+        <div className="nav-button" onClick={randomJoke}>
+          Tell me a joke!
         </div>
+        <div className="rando-joke">
+            {jokeAlert}
+        </div>
+      </div>
     )
 }
 
--- src/data/jokedata.js
+++ src/data/jokedata.js
@@ -2,8 +2,7 @@ export default [
     {
         jokeQ: `Q: Which shape has 4 sides and greatly overestimated the size of the Atlantic Ocean?`, jokeA: `A: Christopher CO-RHOMBUS!`,
         explanation:
-          `Explanation: Christopher Columbus thought the Earth was half the size it actually is. Most people would not attempt the journey he did because they (correctly) assumed their ships wouldn’t hold enough food and water for the voyage. Fortunately for Columbus a previously unknown land mass was discovered, saving his crew from disaster. 
-          Also, a rhombus is a 4 sided shape defined as a parallelogram with opposite equal acute angles, opposite equal obtuse angles, and four equal sides.
+          `Explanation:\nChristopher Columbus thought the Earth was half the size it actually is. Most people would not attempt the journey he did because they (correctly) assumed their ships wouldn’t hold enough food and water for the voyage. Fortunately for Columbus a previously unknown land mass was discovered, saving his crew from disaster.\nAlso, a rhombus is a 4 sided shape defined as a parallelogram with opposite equal acute angles, opposite equal obtuse angles, and four equal sides.
           `,
           url: ""
     },
@@ -12,7 +11,7 @@ export default [
 
         jokeA: `A: He founded America’s first LIE-brary! `,
         explanation:
-          `Explanation: Ben Franklin was a big believer in forming companies as a way of joining people together in pursuit of large goals. Among these companies was a subscription library in the city of Philadelphia, where patrons could access a large reserve of reading materials. Also as a notorious diplomat and politician Franklin was definitely skilled in the art of lying.`,
+          `Explanation:\nBen Franklin was a big believer in forming companies as a way of joining people together in pursuit of large goals. Among these companies was a subscription library in the city of Philadelphia, where patrons could access a large reserve of reading materials. Also as a notorious diplomat and politician Franklin was definitely skilled in the art of lying.`,
           url: ""
     },
     {
@@ -21,12 +20,12 @@ export default [
         
         jokeA: `A: “I made a RIFLE RIFLE!”`,
         explanation:
-          `Explanation: Benjamin Henry definitely invented the repeating rifle, capable of firing multiple rounds from a single barrel without reloading ammunition. 
+          `Explanation:\nBenjamin Henry definitely invented the repeating rifle, capable of firing multiple rounds from a single barrel without reloading ammunition. 
           Whether or not he had a stutter is unknown.`,
           url: "https://www.amazon.com/U-S-History-Joke-Billy-Peden/dp/B087629N8S"
     },
     {
-        jokeQ: "jeez, kid",
+        jokeQ: "jeez,",
         jokeA: "just buy the book already",
         explanation: "",
         url: "https://www.amazon.com/U-S-History-Joke-Billy-Peden/dp/B087629N8S"
